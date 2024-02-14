c335cda5511411bc6efad11999a909f2ec208555
gkkangk@gmail.com <gkkangk@gmail.com>
2024-02-14 16:26:01 +0300

    Level System Done

---
+++ Assets/Scripts/Commands/OnLevelLoaderCommand.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Commands
+{
+    public class OnLevelLoaderCommand
+    {
+        private Transform _levelHolder;
+        public OnLevelLoaderCommand(Transform levelHolder)
+        {
+            _levelHolder = levelHolder;
+        }
+
+        public void Execute(byte levelIndex)
+        {
+            Object.Instantiate(Resources.Load<GameObject>($"Prefabs/LevelPrefabs/level {levelIndex}")); 
+            //LEVEL SPAWN ETMEK İÇİN HARİKA
+        }
+    }
+}
---
+++ Assets/Scripts/Data/ValueObjects/LevelData.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Data.ValueObjects
+{
+    [Serializable]
+    public struct LevelData
+    {
+        public List<PoolData> Pools;
+
+        public LevelData(List<PoolData> pools) //constructor örneği !
+        {
+            Pools = pools;
+        }
+    }
+}
